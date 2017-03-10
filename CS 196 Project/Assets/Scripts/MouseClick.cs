b8f036d3a207f8dc631fc7befaeb3b745df97232
Darren Lim <Darren Lim>
2017-03-10 16:49:24 -0600

    Unity Project

---
+++ CS 196 Project/Assets/Scripts/MouseClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour {
+
+    public bool clicked;
+
+    // Use this for initialization
+    void Start()
+    {
+        clicked = false;
+	}
+	
+	void OnMouseDown()
+    {
+        Debug.Log("clicked");
+    }
+
+}
