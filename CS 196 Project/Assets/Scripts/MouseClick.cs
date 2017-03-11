93401980d7364034cbedae774bd2e0b02be1eb9b
Darren Lim <Darren Lim>
2017-03-11 05:37:18 -0600

    1st change

--- CS 196 Project/Assets/Scripts/MouseClick.cs
+++ CS 196 Project/Assets/Scripts/MouseClick.cs
@@ -12,9 +12,46 @@ public class MouseClick : MonoBehaviour {
         clicked = false;
 	}
 	
+    void Update()
+    {
+        if (GetComponent<Rigidbody2D>().angularVelocity != 0)
+        {
+            GetComponent<Rigidbody2D>().angularVelocity += 2.3f;
+        }
+
+    }
+
 	void OnMouseDown()
     {
-        Debug.Log("clicked");
+        if (!clicked)
+        {
+            GetComponent<SpriteRenderer>().material.color = Color.green;
+            GetComponent<Rigidbody2D>().gravityScale = 0.4f;
+            GetComponent<Rigidbody2D>().angularVelocity = 1;
+            Physics2D.IgnoreCollision(GetComponent<BoxCollider2D>(), GetComponent<BoxCollider2D>());
+            clicked = true;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().material.color = Color.white;
+            clicked = false;
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        if (!clicked)
+        {
+            GetComponent<SpriteRenderer>().material.color = Color.red;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        if (!clicked)
+        {
+            GetComponent<SpriteRenderer>().material.color = Color.white;
+        }
     }
 
 }
