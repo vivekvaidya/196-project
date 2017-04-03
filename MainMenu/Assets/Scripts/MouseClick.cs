32d11552f9993b1707242c669a672b6e7701b745
Darren Lim <Darren Lim>
2017-04-03 18:36:19 -0500

    merged

---
+++ MainMenu/Assets/Scripts/MouseClick.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour {
+
+    public bool clicked;
+
+    // Use this for initialization
+    void Start()
+    {
+        clicked = false;
+	}
+	
+    void Update()
+    {
+        if (GetComponent<Rigidbody2D>().angularVelocity > 0)
+        {
+            GetComponent<Rigidbody2D>().angularVelocity += 2.3f;
+        }
+        else if (GetComponent<Rigidbody2D>().angularVelocity < 0)
+        {
+            GetComponent<Rigidbody2D>().angularVelocity -= 2.3f;
+        }
+
+    }
+
+	void OnMouseDown()
+    {
+        if (!clicked)
+        {
+            GetComponent<SpriteRenderer>().material.color = Color.green;
+            GetComponent<Rigidbody2D>().gravityScale = 0.4f;
+            GetComponent<Rigidbody2D>().angularVelocity = Random.Range(-0.1f,0.1f);
+            Physics2D.IgnoreCollision(GetComponent<BoxCollider2D>(), GetComponent<BoxCollider2D>());
+            clicked = true;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().material.color = Color.white;
+            clicked = false;
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        if (!clicked)
+        {
+            GetComponent<SpriteRenderer>().material.color = Color.red;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        if (!clicked)
+        {
+            GetComponent<SpriteRenderer>().material.color = Color.white;
+        }
+    }
+
+}
