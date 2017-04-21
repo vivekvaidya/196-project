d1529f357bc5d9583e734a1ce3106964496ae9ee
Darren Lim <Darren Lim>
2017-04-21 15:06:43 -0500

    changed sprites

--- MainMenu/Assets/Scripts/MouseClick.cs
+++ MainMenu/Assets/Scripts/MouseClick.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MouseClick : MonoBehaviour {
-
+    public Sprite block_green, block_yellow, block_red;
     public bool clicked;
 
     // Use this for initialization
     void Start()
     {
         clicked = false;
-	}
+    }
 	
     void Update()
     {
+        if (clicked)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                GetComponent<Rigidbody2D>().gravityScale = 0.4f;
+                GetComponent<Rigidbody2D>().angularVelocity = Random.Range(-0.1f, 0.1f);
+                Physics2D.IgnoreCollision(GetComponent<BoxCollider2D>(), GetComponent<BoxCollider2D>());
+            }
+        }
         if (GetComponent<Rigidbody2D>().angularVelocity > 0)
         {
             GetComponent<Rigidbody2D>().angularVelocity += 2.3f;
@@ -29,15 +38,12 @@ public class MouseClick : MonoBehaviour {
     {
         if (!clicked)
         {
-            GetComponent<SpriteRenderer>().material.color = Color.green;
-            GetComponent<Rigidbody2D>().gravityScale = 0.4f;
-            GetComponent<Rigidbody2D>().angularVelocity = Random.Range(-0.1f,0.1f);
-            Physics2D.IgnoreCollision(GetComponent<BoxCollider2D>(), GetComponent<BoxCollider2D>());
+            GetComponent<SpriteRenderer>().sprite = block_green;
             clicked = true;
         }
         else
         {
-            GetComponent<SpriteRenderer>().material.color = Color.white;
+            GetComponent<SpriteRenderer>().sprite = block_yellow;
             clicked = false;
         }
     }
@@ -46,7 +52,7 @@ public class MouseClick : MonoBehaviour {
     {
         if (!clicked)
         {
-            GetComponent<SpriteRenderer>().material.color = Color.red;
+            GetComponent<SpriteRenderer>().sprite = block_red;
         }
     }
 
@@ -54,7 +60,7 @@ public class MouseClick : MonoBehaviour {
     {
         if (!clicked)
         {
-            GetComponent<SpriteRenderer>().material.color = Color.white;
+            GetComponent<SpriteRenderer>().sprite = block_yellow;
         }
     }
 
